3336b144dd287023039650c85ed5f314e4aa864e
gildemberg-santos <gildemberg.santos@gmail.com>
2022-11-19 14:22:04 -0300

    Reformular arquivo main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "legal-research-automation-go/pkg/services"
+import (
+	"fmt"
+	"legal-research-automation-go/pkg/services"
+	"os"
+)
 
 func main() {
+	args := os.Args[1:]
 	new_pdf := services.ExtractPDF{}
-	new_pdf.New("<File Name>.pdf")
+	new_pdf.New(args[0])
 	new_pdf.Open()
+	fmt.Println(new_pdf.Text)
 }
